58eb74ab38913a288f472a1d327dcf70889142db
Annaraju <annaraju311@gmail.com>
2024-04-21 16:13:58 +0530

    shopping app created

---
+++ src/components/AddProduct.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import Header from './Header'
+
+const AddProduct = () => {
+  return (
+    <div>
+        <Header/>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <b><center><h1>ADD NEW RECIPES</h1></center></b>
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Product title</label>
+                            <input type="text" className="form-control"/>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Image</label>
+                            <input type="file" name="" id="" className="form-control"/>
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <label htmlFor="" className="form-label">Description</label>
+                            <textarea name="" id="" cols="30" rows="10" className="form-control"></textarea>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Category</label>
+                            <select name="" id="" className="form-control">
+                                <option value="clothes">clothes</option>
+                                <option value="watches">watches</option>
+                                <option value="laptops">laptops</option>
+                                <option value="accesories">accesories</option>
+                            </select>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">price</label>
+                            <input type="text" className="form-control"/>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <button className="btn btn-success">SUBMIT</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AddProduct
---
+++ src/components/SearchProduct.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Header from './Header'
+
+const SearchRecipe = () => {
+  return (
+    <div>
+        <Header/>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <b><center><h1>SEARCH FOR RECIEPES</h1></center></b>
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <label htmlFor="" className="form-abel">Recipe Title</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <button className="btn btn-primary">SEARCH</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default SearchRecipe
